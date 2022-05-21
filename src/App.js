948d3e2a284ffb5042c6ee6847c9a7c682ae89c2
sabrynnall <limasabrynna03@gmail.com>
2022-05-20 22:24:57 -0300

    consumindo api com react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from 'axios';
 
 function App() {
+  const [ usuario, setUsuario ] = useState('');
+  function handlePesquisa() {
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)} />
+      <button type="button" onClick={handlePesquisa}>Pesquisar</button>
+    </>
   );
 }
 
 export default App;
+
+
